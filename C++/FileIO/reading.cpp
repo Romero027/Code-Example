813606adb18b107861751a27173d98f62c490d6d
xzhu27 <xzhu27@ucsc.edu>
2017-11-05 14:47:49 -0800

    update

---
+++ C++/FileIO/reading.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+
+	string inFileName = "text.txt";
+	//we can use ifstream or fstream
+	ifstream inFile;
+
+	inFile.open(inFileName);
+
+	if (inFile.is_open()) {
+
+		string line;
+		//inFile means !inFile.eof()
+		while (inFile) {
+			getline(inFile, line);
+			cout << line << endl;	
+		}
+
+		inFile.close();
+	} else {
+		cout << "Cannot open file: " << inFileName << endl;
+	}
+
+	return 0;
+}
