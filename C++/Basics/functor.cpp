09541b97bb118390cbf237578945a7630fb9b61f
xzhu27 <xzhu27@ucsc.edu>
2017-11-06 10:32:41 -0800

    update

--- C++/Basics/functor.cpp
+++ C++/Basics/functor.cpp
@@ -14,24 +14,27 @@ private:
 };
 
 int main(){
-	add_x add42(42); 
-	int i = add42(8);
-	cout<<i<<endl;
+
+	add_x add42(42);//This creates an instance of functor class
+	int i = add42(8);//and we can call it
+	cout<<i<<endl;//i is 50 now 
 
 	std::vector<int> foo;
   	std::vector<int> bar;
 
   	// set some values:
   	for (int i=1; i<6; i++)
-   		foo.push_back (i*10);                         // foo: 10 20 30 40 50
+   		foo.push_back (i*10);   // foo: 10 20 30 40 50
 
-  	bar.resize(foo.size());                         // allocate space
+  	bar.resize(foo.size());    // allocate space
 
+  	// Pass a functor to std::transform, which calls the functor on every element 
+	// in the input sequence, and stores the result to the output sequence
   	transform (foo.begin(), foo.end(), bar.begin(), add_x(1));
+  										// bar: 11 21 31 41 51
 
-
-    for (std::vector<int>::iterator it=bar.begin(); it!=bar.end(); ++it)
-    	std::cout << ' ' << *it;
-  		std::cout << '\n';                         	                    
+    for (auto it=bar.begin(); it!=bar.end(); ++it)
+    	cout << *it<< ' ';
+  		cout <<'\n';                         	                    
 
 }
