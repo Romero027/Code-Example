5c55607b6d90149942bfdb8d5a6c3e890670fe31
xzhu27 <xzhu27@ucsc.edu>
2017-11-07 11:17:18 -0800

    update

--- C++/STL/Vector/twoDVector.cpp
+++ C++/STL/Vector/twoDVector.cpp
@@ -9,12 +9,21 @@ int main() {
 
 	grid[1].push_back(8);
 
+	//Method1
 	for(int row=0; row < grid.size(); row++) {
 			for(int col=0; col < grid[row].size(); col++) {
 				cout << grid[row][col] << flush;
 			}
 
 			cout << endl;
+	}
+
+	//Method2
+	for(auto i : grid){
+		for(auto j : i){
+			cout << j << flush;
+		}
+		cout << endl;
 	}
 
 	return 0;
