3cd477bf17d1ca259f375ffba6548e041470c040
xzhu27 <xzhu27@ucsc.edu>
2017-11-08 10:53:15 -0800

    update

---
+++ C++/STL/list/list.cpp
@@ -0,0 +1,7 @@
+//A program show how to use STL list
+//List containers are implemented as doubly-linked lists. Note that list does not support random access
+//For more information about list:http://www.cplusplus.com/reference/list/list/
+
+
+#include<iostream>
+#include<list>
