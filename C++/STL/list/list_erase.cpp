63e35f19af5bb8a738cb9b108d26c88843aeadc6
xzhu27 <xzhu27@ucsc.edu>
2017-11-08 11:30:40 -0800

    update

--- C++/STL/list/list_erase.cpp
+++ C++/STL/list/list_erase.cpp
@@ -12,30 +12,53 @@ int main() {
 	list<int> numbers;
 	// set some values:
   	for (int i=1; i<10; ++i) numbers.push_back(i*10);
-	
-	
-	for(list<int>::iterator it=numbers.begin(); it != numbers.end();) {
-		//cout<<*it<<endl;
-		if(*it == 20){
-			//because this will increment the iterator, so we do not what to 
-			//increment it again
-			it = numbers.erase(it);
-		}else{
-			//it++;
-		}
-		if(*it == 20){
-			//because this will increment the iterator, so we do not what to 
-			//increment it again
-			it = numbers.erase(it);
-		}else{
-			//it++;
-		}
+  	cout<<"Case1(CORRECT)"<<endl;
+	cout<<"Original list"<<endl;
+	for(list<int>::iterator it=numbers.begin(); it != numbers.end(); it++) {
+		cout << *it << endl;
 	}
-	cout<<"------------"<<endl;
+	//We can use for loop, but this is just for demonstration
+	for(list<int>::iterator it=numbers.begin(); it != numbers.end();) {
+		if(*it == 30){
+			it = numbers.erase(it);
+		}
+		if(*it == 20){
+			//because this will increment the iterator, so we do not what to 
+			//increment it again
+			it = numbers.erase(it);
+		}else{
+			it++;
+		}
+		
+	}
+	
+	cout<<"After remove 20 and 30"<<endl;
 	for(list<int>::iterator it=numbers.begin(); it != numbers.end(); it++) {
 		cout << *it << endl;
 	}
 
 
+	cout<<"Case 2(ERROR!)"<<endl;
+	//ERROR!!!!!
+	numbers.clear();//clear the list first
+	for (int i=1; i<10; ++i) numbers.push_back(i*10);
+	cout<<"Original list"<<endl;
+	for(list<int>::iterator it=numbers.begin(); it != numbers.end(); it++) {
+		cout << *it << endl;
+	}
+	for(list<int>::iterator it=numbers.begin(); it != numbers.end();it++) {
+		//In this case, we are not able to remove 30,because we increment the iterator twice
+		if(*it == 30){
+			it = numbers.erase(it);
+		}
+		if(*it == 20){
+			it = numbers.erase(it);
+		}
+		
+	}
+	cout<<"After remove 20 and 30"<<endl;
+	for(list<int>::iterator it=numbers.begin(); it != numbers.end(); it++) {
+		cout << *it << endl;
+	}
 	return 0;
 }
