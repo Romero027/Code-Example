7c77864745db905bca3a432090cc10b643ff74ef
xzhu27 <xzhu27@ucsc.edu>
2017-11-09 23:34:59 -0800

    update

--- C/Basics/getopt.c
+++ C/Basics/getopt.c
@@ -6,44 +6,32 @@
 int
 main (int argc, char **argv)
 {
-  int aflag = 0;
-  int bflag = 0;
-  char *cvalue = NULL;
-  int index;
+ //  int aflag = 0;
+//   int bflag = 0;
+//   char *cvalue = NULL;
+//   int index;
   int c;
 
-  opterr = 0;
+  //opterr = 0;
 
   while ((c = getopt (argc, argv, "abc:")) != -1)
     switch (c)
       {
       case 'a':
-        aflag = 1;
+        printf("You entered a");
         break;
       case 'b':
-        bflag = 1;
+        printf("You entered b");
         break;
-      case 'c':
-        cvalue = optarg;
-        break;
-      case '?':
-        if (optopt == 'c')
-          fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-        else if (isprint (optopt))
-          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-        else
-          fprintf (stderr,
-                   "Unknown option character `\\x%x'.\n",
-                   optopt);
-        return 1;
       default:
-        abort ();
+        printf("This is not!!");
+        return 1;
       }
-
-  printf ("aflag = %d, bflag = %d, cvalue = %s\n",
-          aflag, bflag, cvalue);
-
-  for (index = optind; index < argc; index++)
-    printf ("Non-option argument %s\n", argv[index]);
+// 
+//   printf ("aflag = %d, bflag = %d, cvalue = %s\n",
+//           aflag, bflag, cvalue);
+// 
+//   for (index = optind; index < argc; index++)
+//     printf ("Non-option argument %s\n", argv[index]);
   return 0;
 }
