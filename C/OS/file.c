8893608ea3ad95de8e1a34646bb5a2dc1e00535e
xzhu27 <xzhu27@ucsc.edu>
2017-11-10 20:58:30 -0800

    update

--- C/OS/file.c
+++ C/OS/file.c
@@ -5,6 +5,7 @@
 #include<sys/types.h>
 #include<sys/types.h>
 #include<sys/stat.h>
+#include<sys/uio.h>
 
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 
@@ -12,13 +13,15 @@
 int main(int argc, char *argv[]){
 
 	int fd;
-
+	char buf[14];
+	char str[13] = "Hello World!\n";
 	/* open */
 	//use | operator to allow multiple options(See man 2 open for more detail)
-	fd = open("test.txt", O_CREAT|O_WRONLY|O_APPEND, S_IRWXU|S_IRWXG|S_IRWXO);
+	//last argument sets the file permission(See man 2 chmod)
+	fd = open("test.txt", O_CREAT|O_WRONLY|O_APPEND, S_IRUSR|S_IWUSR);
 
 	if(fd == -1){
-		printf("Failed to create and open the file");
+		printf("Failed to create and open the file\n");
 		exit(1);
 	}
 
@@ -27,19 +30,31 @@ int main(int argc, char *argv[]){
 	//write(int fildes, const void *buf, size_t nbyte);
 	//write() attempts to write nbyte of data to the object referenced by the descriptor fildes from the
     //buffer pointed to by buf.(See man 2 write)
-	write(fd,"Hello World!\n",13);
+	write(fd,str,13);
 
-
-	//close(fd);
-
-	/* open */
-	//fd = open("test.txt", O_CREAT|O_WRONLY|O_APPEND, 0600);
 
 	close(fd);
 
+	/* open */
+	fd = open("test.txt", O_RDONLY);
 
 
+	if(fd == -1){
+		printf("Failed to open and read the file\n");
+		exit(1);
+	}
 
+
+	/* read */
+	//read(int fildes, void *buf, size_t nbyte);
+	//read() attempts to read nbyte bytes of data from the object referenced by the descriptor fildes into the
+    //buffer pointed to by buf.
+	read(fd,buf,13);
+	buf[13] = '\0';
+
+	close(fd);
+
+	printf("buf: %s\n",buf);
 
 	return 0;
 }
