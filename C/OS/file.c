8f3bb9c5db2913892f78d9851a934a7c378859da
xzhu27 <xzhu27@ucsc.edu>
2017-11-10 20:38:23 -0800

    update

--- C/OS/file.c
+++ C/OS/file.c
@@ -2,6 +2,10 @@
 #include<fcntl.h>
 #include<unistd.h>
 #include<stdlib.h>
+#include<sys/types.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 
 
@@ -11,7 +15,7 @@ int main(int argc, char *argv[]){
 
 	/* open */
 	//use | operator to allow multiple options(See man 2 open for more detail)
-	fd = open("test.txt", O_CREAT|O_WRONLY, 0600);
+	fd = open("test.txt", O_CREAT|O_WRONLY|O_APPEND, S_IRWXU|S_IRWXG|S_IRWXO);
 
 	if(fd == -1){
 		printf("Failed to create and open the file");
@@ -20,7 +24,16 @@ int main(int argc, char *argv[]){
 
 
 	/* write */
-	write(fd,"Hello World!\n",11);
+	//write(int fildes, const void *buf, size_t nbyte);
+	//write() attempts to write nbyte of data to the object referenced by the descriptor fildes from the
+    //buffer pointed to by buf.(See man 2 write)
+	write(fd,"Hello World!\n",13);
+
+
+	//close(fd);
+
+	/* open */
+	//fd = open("test.txt", O_CREAT|O_WRONLY|O_APPEND, 0600);
 
 	close(fd);
 
