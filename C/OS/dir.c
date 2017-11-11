0407edf9ea7c29896fe47c811ad71bf6d454c83d
xzhu27 <xzhu27@ucsc.edu>
2017-11-11 11:50:37 -0800

    update

--- C/OS/dir.c
+++ C/OS/dir.c
@@ -7,12 +7,13 @@ int main(){
 
 	int ret;
 
-
+	/* create a directory */*
+	
 	//The first argument is the name of path and the second arguemnt is the mode(See man 2 mkdir)
 	// ret = mkdir("Mynewdir",
 	// 	S_IRUSR|S_IRGRP|S_IROTH|S_IWUSR|S_IXUSR|S_IXGRP|S_IXOTH);
 
-
+	/* remove a directory */
 	ret = rmdir("Mynewdir");
 
 	return 0;
