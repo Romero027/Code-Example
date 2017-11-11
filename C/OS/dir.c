41e042b5ad5100eff472c232de18fe522dd23bdd
xzhu27 <xzhu27@ucsc.edu>
2017-11-11 11:40:25 -0800

    update

--- C/OS/dir.c
+++ C/OS/dir.c
@@ -1,7 +1,7 @@
 //A program show that how to create and remove a directory
 #include<stdio.h>
 #include<sys/stat.h>
-
+#include<unistd.h>
 
 int main(){
 
@@ -9,7 +9,11 @@ int main(){
 
 
 	//The first argument is the name of path and the second arguemnt is the mode(See man 2 mkdir)
-	ret = mkdir("Mynewdir",0000777);
+	// ret = mkdir("Mynewdir",
+	// 	S_IRUSR|S_IRGRP|S_IROTH|S_IWUSR|S_IXUSR|S_IXGRP|S_IXOTH);
+
+
+	ret = rmdir("Mynewdir");
 
 	return 0;
 }
