47fb7591e6f6cebb41f7dccdd752cd3532ad2ced
xzhu27 <xzhu27@ucsc.edu>
2017-11-11 11:25:16 -0800

    update

--- C/OS/read.c
+++ C/OS/read.c
@@ -5,11 +5,21 @@ int main(){
 	
 	char buf[10];
 	int ret;
-	//0 is the stdin 
-	ret = read(0,buf,10);/
-	printf("You entered: %s\n", buf);
-	
 
+	while(1){
+		//0 is the stdin 
+		ret = read(0,buf,10);
+		if(ret < 10){
+			//We need to put a null character at the end of the string
+			buf[ret]='\0';
+			printf("You entered: %d of bytes and the string is %s\n",ret, buf);
+			break;
+		}else{
+			printf("You entered: %d of bytes and the string is %s\n",ret, buf);
+		}
+	}
+	
+	
 
 	return 0;
 }
