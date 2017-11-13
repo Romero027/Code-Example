902af38d4a68f7e9559c860bbe0d429f6bcdcfa4
Xiangfeng(Allen) Zhu <xzhu27@ucsc.edu>
2017-11-12 23:41:49 -0800

    update

---
+++ C/OS/system.c
@@ -0,0 +1,15 @@
+//system -- pass a command to the shell(See man 3 system)
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	
+	int ret;
+
+	printf("Calling system...\n");
+
+	ret = system("ls -l");
+
+	printf("Exiting system... return value is %d\n",ret);
+	return 0;
+}
