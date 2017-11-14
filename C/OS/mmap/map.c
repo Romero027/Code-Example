c167d64cbfe5c05302f0dc4153a9e8522e4c245d
Xiangfeng(Allen) Zhu <xzhu27@ucsc.edu>
2017-11-13 23:08:08 -0800

    update

--- C/OS/mmap/map.c
+++ C/OS/mmap/map.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <sys/mman.h>
+#include <sys/types.h>
 
 int main (int argc, char **argv)
 {
-  int fd 
+  int fd,pagesize;
+
+  //Before mapping a file to memory
+  fd = open("test.txt",O_RDWR);
+  pagesize = getpagesize();
+
+
   return 0;
 }
