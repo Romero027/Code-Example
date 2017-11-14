f09701ff56b2a6319406529736b6c379d56a9da9
Xiangfeng(Allen) Zhu <xzhu27@ucsc.edu>
2017-11-13 23:15:16 -0800

    update

--- C/OS/mmap/map.c
+++ C/OS/mmap/map.c
@@ -5,11 +5,16 @@
 
 int main (int argc, char **argv)
 {
-  int fd,pagesize;
 
-  //Before mapping a file to memory
-  fd = open("test.txt",O_RDWR);
-  pagesize = getpagesize();
+
+  	int fd,pagesize;
+  	char *data;
+  	//Before mapping a file to memory, we need to get a file descriptor for it
+  	//In this case we use read&write access. You can open it in whatever you want,
+  	//but it has to match the mode sepcified in the prot parameter to the mmap() call
+  	fd = open("test.txt",O_RDWR);
+
+  	pagesize = getpagesize();
 
 
   return 0;
