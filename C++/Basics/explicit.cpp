0656ad555534b7b9ab85320068e48dd4ac4ec0b1
Xiangfeng(Allen) Zhu <xzhu27@ucsc.edu>
2017-11-14 17:18:04 -0800

    update

---
+++ C++/Basics/explicit.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+
+
+class Foo
+{
+public:
+  // single parameter constructor, can be used as an implicit conversion
+  Foo (int foo) : m_foo (foo) 
+  {
+  }
+
+  int GetFoo () { return m_foo; }
+
+private:
+  int m_foo;
+};
+
+void DoBar (Foo foo)
+{
+  int i = foo.GetFoo ();
+}
+
+int main ()
+{
+  DoBar (42);
+}
