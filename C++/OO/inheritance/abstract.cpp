1967d918f42ce0d7d7fcdd854aba5e87b14be1db
Xiangfeng(Allen) Zhu <xzhu27@ucsc.edu>
2017-11-15 17:13:20 -0800

    update

--- C++/OO/inheritance/abstract.cpp
+++ C++/OO/inheritance/abstract.cpp
@@ -1,14 +1,18 @@
+//https://www.tutorialspoint.com/cplusplus/cpp_interfaces.htm
 
 #include <iostream>
 using namespace std;
-
+//A class is made abstract by declaring at least one of its functions as pure virtual function.
+//Animal is called abstract  class(Attempting to instantiate an object of an abstract class causes a compilation error.)
 class Animal {
 public:
-	//pure virtual function
-	virtual void run()=0;
+	//pure virtual function(A pure virtual function is specified by placing "= 0" )
+	virtual void run() = 0;
 	virtual void speak() = 0;
 };
 
+
+//Classes that can be used to instantiate objects are called concrete classes.
 class Dog: public Animal {
 public:
 	virtual void speak() {
@@ -44,7 +48,7 @@ void test(Animal &a) {
 }
 
 int main() {
-
+	Animal a;
 	Labrador lab;
 	lab.run();
 	lab.speak();
