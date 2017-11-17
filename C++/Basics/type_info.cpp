d635edf96664e32ec2aa4d3fa78adaa8ba56af8d
Xiangfeng(Allen) Zhu <xzhu27@ucsc.edu>
2017-11-16 19:49:40 -0800

    update

--- C++/Basics/type_info.cpp
+++ C++/Basics/type_info.cpp
@@ -3,15 +3,17 @@
 #include <iostream>       // std::cout
 #include <typeinfo>       // operator typeid
 
+using namespace std;
+
 int main() {
 	int i;
   	int * pi;
-  	std::string value;
-  	std::cout << "int is: " << typeid(int).name() << '\n';
-  	std::cout << "  i is: " << typeid(i).name() << '\n';
-  	std::cout << " pi is: " << typeid(pi).name() << '\n';
-  	std::cout << "*pi is: " << typeid(*pi).name() << '\n';
-   	std::cout << "value is: " << typeid(value).name() << '\n';
+  	string value;
+  	cout << "int is: " << typeid(int).name() << '\n';
+  	cout << "  i is: " << typeid(i).name() << '\n';
+  	cout << " pi is: " << typeid(pi).name() << '\n';
+  	cout << "*pi is: " << typeid(*pi).name() << '\n';
+   	cout << "value is: " << typeid(value).name() << '\n';
 
    	//It inspects the declared type of an entity or the type of an expression. Auto lets you declare a variable with particular type whereas decltype lets you extract 
    	//the type from the variable so decltype is sort of an operator that evaluates the type of passed expression.
@@ -23,6 +25,6 @@ int main() {
    	cout<< typeid(value).name()<<endl;
 
    	cout<<name<<endl;
-   	
+
   	return 0;
 }
