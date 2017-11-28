827bbd4c437aaf504c1d5b97faf45a5c9225c947
Xiangfeng(Allen) Zhu <xzhu27@ucsc.edu>
2017-11-27 16:33:05 -0800

    update

--- C/exit/atexit.c
+++ C/exit/atexit.c
@@ -1,5 +1,5 @@
 //Source:http://www.cplusplus.com/reference/cstdlib/atexit/
-
+//See man 3 at exit for more detail
 
 /* atexit example */
 #include <stdio.h>      /* puts */
@@ -17,6 +17,9 @@ void fnExit2 (void)
 
 int main ()
 {
+  //The atexit funciton registers the given function to be called at program exit,
+  //wether via exit or via return from the program's main(). Functions so registered are 
+  //called in reverse order; no argument are passed
   atexit (fnExit1);
   atexit (fnExit2);
   puts ("Main function.");
