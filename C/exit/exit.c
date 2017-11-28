c22c8af6b1a595ed32c436b5296ab5c5a5613e40
Xiangfeng(Allen) Zhu <xzhu27@ucsc.edu>
2017-11-27 16:22:09 -0800

    update

---
+++ C/exit/exit.c
@@ -0,0 +1,21 @@
+//Source:GeekforGeeks
+
+/* exit example */
+#include <stdio.h>
+#include <stdlib.h>
+  
+int main ()
+{
+  FILE * pFile;
+  pFile = fopen ("myfile.txt", "r");
+  if (pFile == NULL)
+  {
+    printf ("Error opening file\n");
+    exit (1);
+  }
+  else
+  {
+    /* file operations here */
+  }
+  return 0;
+}
