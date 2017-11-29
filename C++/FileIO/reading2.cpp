16b94cc3df315fe4c214ba05284426228594ede3
Xiangfeng(Allen) Zhu <xzhu27@ucsc.edu>
2017-11-28 16:00:26 -0800

    update

--- C++/FileIO/reading2.cpp
+++ C++/FileIO/reading2.cpp
@@ -2,31 +2,36 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
+#include <assert.h>
 
 using namespace std;
 
+void readfile(ifstream& infile, vector<string>& lines){
+	assert(infile.good());
+
+	string line;
+	while(infile.peek()!=EOF){
+		getline(infile,line);
+		lines.push_back(line);
+	}
+	cout<<"All done! (Number of lines: "<<lines.size()<<")"<<endl;
+}
+
+
 int main() {
 
-	
 	string inFileName = "text.txt";
 	//we can use ifstream or fstream
+	vector<string> lines;
 	ifstream inFile;
-
 	inFile.open(inFileName);
+	readfile(inFile, lines);
 
-	if (inFile.is_open()) {
-
-		string line;
-		//inFile means !inFile.eof()
-		while (inFile) {
-			getline(inFile, line);
-			cout << line << endl;	
-		}
-
-		inFile.close();
-	} else {
-		cout << "Cannot open file: " << inFileName << endl;
+	for(auto i : lines){
+		cout<<i<<endl;
 	}
+
 
 	return 0;
 }
