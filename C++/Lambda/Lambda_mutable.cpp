8891c8d32ce63d3e258ef317b2006f1b2dee94da
Xiangfeng(Allen) Zhu <xzhu27@ucsc.edu>
2017-12-25 15:54:14 +0800

    update

--- C++/Lambda/Lambda_mutable.cpp
+++ C++/Lambda/Lambda_mutable.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
 
 	int cats = 5;
-	//cannot assign to a variable captured by copy in a non-mutable lambda(if we don have mutabel)
+	//cannot assign to a variable captured by copy in a non-mutable lambda(if we don have mutabe)
 	//We can also pass reference in this case, but it will change the value of cats, print 8 8
 	[cats]() mutable {
 		cats = 8;
